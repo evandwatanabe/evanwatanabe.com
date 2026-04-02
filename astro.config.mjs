ac4c521a31060ad35ae5138fb915f69511d20a49
Evan Watanabe <evanwatanabe@Mac.localdomain>
2026-04-02 10:59:04 -0400

    Simplify project for Vercel deployment and remove GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://evandwatanabe.github.io',
-  base: '/evanwatanabe.com/',
   integrations: [tailwind()]
 });
