6848545b7384ba4522ba60cc207793b49538689e
Evan Watanabe <evanwatanabe@Mac.localdomain>
2026-04-02 09:41:32 -0400

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {},
+      fontFamily: {
+        sans: ['"Helvetica Neue"', 'Helvetica', 'Arial', 'sans-serif'],
+        serif: ['"Helvetica Neue"', 'Helvetica', 'Arial', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
