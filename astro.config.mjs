b87d66577638156e04f525e58e85e2e1b0dbf81f
Evan Watanabe <evanwatanabe@Mac.localdomain>
2026-04-02 10:24:22 -0400

    Fix Astro base URL for Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://evandwatanabe.github.io',
+  base: '/evanwatanabe.com',
   integrations: [tailwind()]
 });
